ba507ba87f29633b2a73fc03d9c17cc39298bd4e
Jorge Gómez Tortosa <gmzjordi@gmail.com>
2020-05-08 21:22:17 +0200

    Clase Pasaporte

--- PapersPlease/PapersPlease/MainWindow.xaml.cs
+++ PapersPlease/PapersPlease/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PapersPlease
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ PapersPlease/PapersPlease/Pasaporte.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapersPlease
+{
+    class Pasaporte
+    {
+        string nombre;
+        string apellidos;
+        string dni;
+        DateTime fechaNacimiento;
+
+        public Pasaporte():this("", "", "", DateTime.Now) { }
+        public Pasaporte(string nombre, string apellidos, string dni, DateTime fechaNacimiento)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.dni = dni;
+            this.fechaNacimiento = fechaNacimiento;
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public string GetApellidos()
+        {
+            return apellidos;
+        }
+        public void SetApellidos(string apellidos)
+        {
+            this.apellidos = apellidos;
+        }
+
+        public string GetDni()
+        {
+            return dni;
+        }
+        public void SetDni(string dni)
+        {
+            this.dni = dni;
+        }
+
+        public DateTime GetFechaNacimiento()
+        {
+            return fechaNacimiento;
+        }
+        public void GetFechaNacimiento(DateTime fechaNacimiento)
+        {
+            this.fechaNacimiento = fechaNacimiento;
+        }
+    }
+}
